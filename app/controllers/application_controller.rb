0d6c10c28957f6f2a4e8dff1e3dc74c2c00e68d4
Richi Hariyanto <richihariyanto90@gmail.com>
2025-02-24 10:02:09 +0700

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   
+  include SessionsHelper
+  
   def hello
     render html: "hello, world!"
   end
