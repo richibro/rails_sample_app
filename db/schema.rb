6ac0287ad4d1d9e35b0cb7b02b705fd8462d5524
Richi Hariyanto <richihariyanto90@gmail.com>
2025-02-25 09:06:20 +0700

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_010245) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_032208) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,11 @@ module ActiveSupport
     def is_logged_in?
       !session[:user_id].nil?
     end
+    
+    # Log in as a particular user.
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
 
     # Add more helper methods to be used by all tests here...
   end
